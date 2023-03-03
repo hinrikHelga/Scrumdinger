c81ef6372e50e21db33fd93c06e225027363b57a
Hinrik Helgason <hinrik.helga@gmail.com>
2023-03-03 15:28:42 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Hinrik Helgason on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
